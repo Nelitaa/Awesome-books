f54791638574c2a4b5f52de383d496f0e562c50b
KMkansi <KMkansi@BSS-L-VMAKOLE>
2022-10-24 22:35:16 +0200

    Create a function to add a new book to the collection

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const books = [
   {title: "The Catcher in the Rye", author: "J. D. Salinger"},
 ];
 
+const addBooks = document.getElementById("addButton");
 const divSectionBooks = document.createElement('div');
 document.getElementById('section-books').appendChild(divSectionBooks);
 
@@ -22,3 +23,28 @@ for (let i=0; i<books.length; i += 1) {
   const hr = document.createElement("hr");
   divSectionBooks.appendChild(hr);
 }
+
+addBooks.addEventListener('click', (e) => {
+  e.preventDefault();
+  const bookTitle = document.getElementById('title').value;
+  const bookAuthor = document.getElementById('author').value;
+
+  books.push({"title" : bookTitle, "author" :bookAuthor});
+
+  const title = document.createElement("p");
+  title.textContent = bookTitle;
+  divSectionBooks.appendChild(title);
+  
+  const author = document.createElement("p");
+  author.textContent = bookAuthor;
+  divSectionBooks.appendChild(author);
+  
+  const buttonRemove = document.createElement("button");
+  buttonRemove.textContent = "Remove";
+  divSectionBooks.appendChild(buttonRemove);
+  
+  const hr = document.createElement("hr");
+  divSectionBooks.appendChild(hr);
+  
+
+})
