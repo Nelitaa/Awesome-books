cb0df7f249134a0943c02cc0d0a6d62bafbb5018
Marienela Munoz Gutierrez <marienelamunozgutierrez@MacBook-Pro-de-Marienela.local>
2022-10-24 16:09:28 -0400

    create script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const books = {
-  "book1": {title: "The Great Gatsby", author: "F. Scott Fitzgerald"},
-  "book2": {title: "The Catcher in the Rye", author: "J. D. Salinger"},
+const books = [
+  {title: "The Great Gatsby", author: "F. Scott Fitzgerald"},
+  {title: "The Catcher in the Rye", author: "J. D. Salinger"},
+];
+
+const divSectionBooks = document.createElement('div');
+document.getElementById('section-books').appendChild(divSectionBooks);
+
+for (let i=0; i<books.length; i += 1) {
+  const title = document.createElement("p");
+  title.textContent = books[i].title;
+  divSectionBooks.appendChild(title);
+  
+  const author = document.createElement("p");
+  author.textContent = books[i].author;
+  divSectionBooks.appendChild(author);
+  
+  const buttonRemove = document.createElement("button");
+  buttonRemove.textContent = "Remove";
+  divSectionBooks.appendChild(buttonRemove);
+  
+  const hr = document.createElement("hr");
+  divSectionBooks.appendChild(hr);
 }
-
-addButton.addEventListener("click", function() {
-
-  let bookTitle = document.getElementById("title").value;
-  let bookAuthor = document.getElementById("author").value;
-  let bookID = document.getElementById("id").value;
-
-  books[bookID] = {title: bookTitle, author: bookAuthor};
-  console.log(books);
-
-});
