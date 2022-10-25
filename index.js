fd5e249c1c635b43d74786b3cbf1bb2c44bb1a9b
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2022-10-25 11:23:36 -0400

    Fix linter errors

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ const init = () => {
 
     books.push(newBook);
     createBook(newBook);
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   });
 };
 init();
